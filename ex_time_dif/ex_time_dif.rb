5c76b2fb4688e470b86f5ac6c8e9e9f63a48a57d
lorenzo <lorenzotijerina@gmail.com>
2022-12-02 11:45:59 -0500

    complete my_min

--- ex_time_dif/ex_time_dif.rb
+++ ex_time_dif/ex_time_dif.rb
@@ -1,1 +1,16 @@
-ready
+def my_min(array)
+    (0...array.length-1).each do |i|
+        return array[i] if array.all? {|ele| ele >= array[i]}
+    end
+end
+
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+puts my_min(list)  # =>  -5
+
+
+def my_min_efficient(array)
+    array.inject {|acc, ele| ele < acc ? ele : acc }
+end
+
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+puts my_min_efficient(list)  # =>  -5
