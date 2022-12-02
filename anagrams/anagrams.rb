bfd5a768705f3f0de8e3bd9530629a180c60569f
lorenzo <lorenzotijerina@gmail.com>
2022-12-02 15:11:16 -0500

    complete first anagram

--- anagrams/anagrams.rb
+++ anagrams/anagrams.rb
@@ -1,20 +1,27 @@
-def first_anagram?(str1, str2)
-    anagrams = []
-    sorted = false
-    dummy = str1.dup 
-    until sorted
-        (0...dummy.length - 1).each do |i|
-            j = i + 1
-            dummy[i], dummy[j] = dummy[j], dummy[i]
-            anagrams << dummy.dup
-        end
-        sorted = true if dummy == str1
-    end
-    anagrams
+# def first_anagram?(str1, str2)
+#     anagrams = []
+#     sorted = false
+#     dummy = str1.dup 
+#     until sorted
+#         (0...dummy.length - 1).each do |i|
+#             j = i + 1
+#             dummy[i], dummy[j] = dummy[j], dummy[i]
+#             anagrams << dummy.dup
+#         end
+#         sorted = true if dummy == str1
+#     end
+#     anagrams
+# end
+
+# puts first_anagram?('elvis', 'lives').inspect
+
+
+
+def second_first_anagram?(str1, str2)
+    perms = str1.chars.permutation.to_a.map(&:join)
+    perms.include?(str2)
+
 end
 
-puts first_anagram?('elvis', 'lives').inspect
-
-
-# anagram?("gizmo", "sally")    #=> false
-# anagram?("elvis", "lives")    #=> true
+p second_first_anagram?("gizmo", "sally")    #=> false
+p second_first_anagram?("elvis", "lives")    #=> true
